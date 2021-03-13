9285d7b1fe38e5a674003951229f69dc01be4641
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-03-13 18:44:16 +0300

    added axios

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 function App() {
   const [coins, setCoins] = useState([])
-  useEffect(()=>{
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=kshs&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
+  useEffect (()=>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
       setCoins(res.data)
       console.log(res.data)
     })
-
-    })
+  })
 
 
   return (
