d3ab98423b2aeb28fca638dcafd6039192896861
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-03-14 10:47:48 +0300

    started adding jsx

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 
 function App() {
   const [coins, setCoins] = useState([])
-  useEffect (()=>{
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res => {
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
       setCoins(res.data)
-      console.log(res.data)
-    })
-  })
-
+    }).catch(error => console.log(error))
+  }, [])
 
   return (
-    <div className="App">
-    <h1> Hello World </h1>
+    <div className="coin-app">
+    <div className = "coin-search">
+      <h1 className="coin-text">such crypto</h1>
+      <form>
+        <input type="text" className="coin-input"/>
+      </form>
 
     </div>
-  )
+  </div>)
 }
 export default App;
