3beaa1116a6796a3400509a728a4ddac339fbe0b
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-03-16 19:27:26 +0300

    added tailwind and dependencies

--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Coin = ({image, name, price, symbol, volume, priceChange}) => {
+const Coin = ({image, name, price, symbol, volume, priceChange, marketcap}) => {
     return (
         <div className="coin-container">
             <div className="coin-row">
@@ -11,14 +11,16 @@ const Coin = ({image, name, price, symbol, volume, priceChange}) => {
                 </div>
                 <div className="coin-data">
                     <p className="coin-price">${price}</p>
-                    <p className="coin-volume">${volume.toLocaleString()}</p>
-                    //change prices color 
+                    <p className="coin-marketcap">${marketcap.toLocaleString()}</p>
                     {priceChange < 0 ? (
                         <p className="coin-percent-red">{priceChange.toFixed(2)}%</p>
                     ) : (
                         <p className="coin-percent-green">{priceChange.toFixed(2)}%</p>
                     )
                 }
+                <p className="coin-volume">
+                    volume: ${volume.toLocaleString()}
+                </p>
                 </div>
             </div>
         </div>
